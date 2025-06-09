8cacce2c6f56d8fab65f2d59272f01bfc78750d9
Samuel Enguix Carbó <samuelenguixcarbo@gmail.com>
2025-06-09 08:50:42 +0200

    proves bloquejar lote si comprat

--- php/loteTenda.php
+++ php/loteTenda.php
@@ -5,12 +5,14 @@ class loteTenda {
     public $preu;
     public $nom;
     public $cosmetics = [];
+    public $usuaris = [];
 
-    public function __construct($id, $preu = 0, $nom = '', $cosmetics = []) {
+    public function __construct($id, $preu = 0, $nom = '', $cosmetics = [], $usuaris = []) {
         $this->id = $id;
         $this->preu = $preu;
         $this->nom = $nom;
         $this->cosmetics = $cosmetics;
+        $this->usuaris = $usuaris;
     }
 
     
